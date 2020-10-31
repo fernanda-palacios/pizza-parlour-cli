f9388c6d5f3f0a5d3741e50266a690f8969f5250
FernandaPalacios <ferpalacios.a@gmail.com>
2020-10-31 11:33:52 -0500

    leave only available commands

--- hello.py
+++ hello.py
@@ -15,9 +15,8 @@ def conn():
     # os.system('curl http://127.0.0.1:5000/')
 
 def main():
-    value = click.prompt('Select a command to run', type=click.Choice(list(cli.commands.keys()) + ['exit']))
-    while value != 'exit':
-        cli.commands[value]()
+    value = click.prompt('Select a command to run', type=click.Choice(list(cli.commands.keys())))
+    cli.commands[value]()
 
 if __name__ == "__main__":
     main()
