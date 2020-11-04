ef07f9a0c425686faac5aed4a68e95b26fb57d0e
Fernanda Palacios <ferpalacios.a@gmail.com>
2020-11-04 10:24:50 -0500

    implement remove topping from pizza cli (#10)

--- cli_commands/order.py
+++ cli_commands/order.py
@@ -65,6 +65,23 @@ def add_topping_to_pizza(order_id, pizza_item_id, topping_item_id):
 
 @order.command()
 @click.option('--order_id')
+@click.option('--pizza_item_id')
+@click.option('--topping_item_id')
+def remove_topping_from_pizza(order_id, pizza_item_id, topping_item_id):
+    url_format = 'http://127.0.0.1:5000/pizzaTopping'
+
+    query_params = {
+        'order_id': order_id,
+        'pizza_item_id': pizza_item_id,
+        'topping_item_id': topping_item_id
+    }
+
+    response = requests.delete(url_format, params=query_params)
+    click.echo(response.text) 
+
+
+@order.command()
+@click.option('--order_id')
 def see_order(order_id):
     url_format = 'http://127.0.0.1:5000/order'
     
@@ -87,3 +104,4 @@ def cancel_order(order_id):
 
     response = requests.delete(url_format, params=query_params)
     click.echo(response.text) 
+    
