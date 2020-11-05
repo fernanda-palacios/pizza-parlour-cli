06793c29e9f1d33b5b0ca687c2ea5efce39e68c9
Fernanda Palacios <ferpalacios.a@gmail.com>
2020-11-05 09:26:33 -0500

    Cli tests - clean cli.py and remove poc cli tests files (#22)

--- tests/cli_tests.py
+++ tests/cli_tests.py
@@ -5,14 +5,14 @@ def test_see_full_menu():
   runner = CliRunner()
   result = runner.invoke(cli, ['menu', 'see-full-menu'])
   assert result.exit_code == 0
-  assert 'menu:' in result.output
+  assert 'Menu:' in result.output
 
 
 def test_item_price():
   runner = CliRunner()
   result = runner.invoke(cli, ['menu', 'item-price', '--item_id', '1'])
   assert result.exit_code == 0
-  assert 'price:' in result.output
+  assert 'Price:' in result.output
 
 
 def test_create_order():
