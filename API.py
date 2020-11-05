e2b7db7f67c8e709afbf574d13c3e2d6bf29d1ab
shiseru <nathaniel.naka@gmail.com>
2020-11-05 17:47:50 +0900

    implemented getItem

--- API.py
+++ API.py
@@ -7,6 +7,19 @@ app = Flask(__name__)
 api = Api(app)
 
 
+class ItemPrice(Resource):
+    def get(self, item_id):
+        price_index = 4
+        with open('./menu.csv') as file:
+            reader = csv.reader(file)
+            contents = [row for row in reader]
+
+        if item_id >= len(contents):
+            return {'message': 'item_id does not exist'}
+
+        return contents[item_id][price_index]
+
+
 class Menu(Resource):
     def get(self):
         with open('./menu.csv') as file:
@@ -30,8 +43,8 @@ class Menu(Resource):
 
         return {'pizzas': pizzas, 'toppings': toppings, 'drinks': drinks}
 
-
 api.add_resource(Menu, '/menu')
+api.add_resource(ItemPrice, '/itemPrice/<int:item_id>')
 
 if __name__ == "__main__":
     app.run()
