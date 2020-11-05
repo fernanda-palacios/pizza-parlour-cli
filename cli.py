6d68795d9a5ab47ff1ab3e9b57e7b2a1b28746f1
FernandaPalacios <ferpalacios.a@gmail.com>
2020-11-05 09:24:42 -0500

    clean cli.py and remove poc cli tests files

--- cli.py
+++ cli.py
@@ -9,35 +9,6 @@ from cli_commands.pickup_or_delivery import pickup_or_delivery
 def cli():
     pass
 
-
-# additional, dummy more complex interaction examples
-@cli.command()
-@click.argument('arg')
-def with_api_request(arg):
-    click.echo('with_api_request')
-    click.echo(arg)
-    url_format = 'http://127.0.0.1:5000/pizza'
-    response = requests.get(url_format)
-    click.echo(response.text) 
-
-
-@cli.command()
-@click.option('--n', default=1, show_default=True)
-# usage: cli dots --n=3
-def dots(n):
-    click.echo('.' * n)
-
-
-@cli.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-              
-def hello(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo('Hello %s!' % name)
-
 def main():
     value = click.prompt('Select a command to run', type=click.Choice(list(cli.commands.keys())))
     cli.commands[value]()
